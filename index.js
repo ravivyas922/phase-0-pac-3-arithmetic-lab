4c4791b978dbf37f8c4260a57693c29eb3db9912
Ravi <ravivyas922@gmail.com>
2022-11-12 18:26:07 -0500

    commit tree d9f0d38d92d023836b48e20430285983b6016773

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a,b)
+{
+    return a+b;
+}
+function subtract(a,b)
+{
+    return a-b;
+}
+function multiply(a,b)
+{
+    return a*b;
+}
+function divide(a,b)
+{
+    return a/b;
+}
+function increment(a)
+{
+    a++;
+    return a;
+}
+function decrement(a)
+{
+    a--;
+    return a;
+}
+function makeInt(string)
+{
+    return parseInt(string,10);
+}
+function preserveDecimal(string)
+{
+    return parseFloat(string);
+}
